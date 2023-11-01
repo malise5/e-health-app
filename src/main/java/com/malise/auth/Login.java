dd72ab7e1e21d9de55834a986a5301631a009b56
Halkano Malise <kudezx@gmail.com>
2023-11-01 22:02:01 +0300

    New Update
    Updated Login and home file that had a bug

--- src/main/java/com/malise/auth/Login.java
+++ src/main/java/com/malise/auth/Login.java
@@ -3,6 +3,7 @@ package com.malise.auth;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -12,27 +13,19 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/login")
 public class Login extends HttpServlet {
 
-  // @Override
-  // protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws
-  // ServletException, IOException {
-
-  // PrintWriter print = resp.getWriter();
-
-  // print.print("../auth/login.html");
-
-  // }
-
   @Override
   protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
     String username = req.getParameter("username");
     String password = req.getParameter("password");
 
-    PrintWriter print = resp.getWriter();
-
     if (username.equals("malise") && password.equals("malise123")) {
-      print.println("<h1>Welcome to Afya Nursing Home</h1>");
+
+      RequestDispatcher dispatcher = req.getRequestDispatcher("./app/home.html");
+      dispatcher.include(req, resp);
+
     } else {
+      PrintWriter print = resp.getWriter();
       print.print("<html><body><h2>Wrong username and password <a href=\".\">Login again</a></h2></body></html>");
     }
 
