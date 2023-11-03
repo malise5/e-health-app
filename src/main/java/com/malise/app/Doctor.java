eea9c2e9a3203f1f68703d357c97efe237e2bf38
Halkano Malise <kudezx@gmail.com>
2023-11-03 15:25:11 +0300

    New Doctor details
    created new Doctors Details

---
+++ src/main/java/com/malise/app/Doctor.java
@@ -0,0 +1,44 @@
+package com.malise.app;
+
+import java.io.Serializable;
+
+public class Doctor implements Serializable {
+
+  private int index;
+  private String name;
+  private String email;
+  private String specialization;
+
+  public int getIndex() {
+    return index;
+  }
+
+  public void setIndex(int index) {
+    this.index = index;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public String getSpecialization() {
+    return specialization;
+  }
+
+  public void setSpecialization(String specialization) {
+    this.specialization = specialization;
+  }
+
+}
--- src/main/java/com/malise/home/Home.java
+++ src/main/java/com/malise/home/Home.java
@@ -21,4 +21,8 @@ public class Home extends HttpServlet {
 
   }
 
+  public String chartOfDoctors() {
+    return null;
+  }
+
 }
