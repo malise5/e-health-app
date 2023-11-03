445beadfffa01de3fcc413736db77c6ac7e8db29
Halkano Malise <kudezx@gmail.com>
2023-11-03 09:32:19 +0300

    Update Home
    created new functionality for home

--- src/main/java/com/malise/auth/Login.java
+++ src/main/java/com/malise/auth/Login.java
@@ -5,29 +5,56 @@ import java.io.PrintWriter;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebInitParam;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/login")
+@WebServlet(urlPatterns = "/login", initParams = {
+    @WebInitParam(name = "username", value = "malise"),
+    @WebInitParam(name = "password", value = "malise123"),
+})
 public class Login extends HttpServlet {
 
   @Override
   protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
+    // Get the "username" and "password" parameters from the request form.
     String username = req.getParameter("username");
     String password = req.getParameter("password");
 
-    if (username.equals("malise") && password.equals("malise123")) {
+    // Check if the entered username and password match the expected values.
+    // if (username.equals("malise") && password.equals("malise123"))
+    if (username.equals(getInitParameter("username")) && password.equals(getInitParameter("password"))) {
 
-      RequestDispatcher dispatcher = req.getRequestDispatcher("./app/home.html");
-      dispatcher.include(req, resp);
+      // If the username and password are correct, create a RequestDispatcher to
+      // forward the request to a different page.
+      // RequestDispatcher dispatcher = req.getRequestDispatcher("./app/home.html");
+      // => redirects to home page
+
+      req.setAttribute("homeinfo", "welcome to E-Health Home page");
+      RequestDispatcher dispatcher = req.getRequestDispatcher("./home");
+      dispatcher.forward(req, resp);
+
+      // Use the RequestDispatcher to include (forward) the request and response
+      // objects to the target page.
+      // dispatcher.include(req, resp);
+
+      // Note: The dispatcher.include(req, resp); Itredirect but includes the content
+      // of the "home.html" page within the current response.
+
+      // ?resp.sendRedirect('./app/home.html') will include the path in the url
 
     } else {
+      // If the entered username and password are incorrect, create a PrintWriter to
+      // send a response to the client.
       PrintWriter print = resp.getWriter();
+
+      // Send an HTML response indicating that the username and password are incorrect
+      // and provide a link to login again.
       print.print("<html><body><h2>Wrong username and password <a href=\".\">Login again</a></h2></body></html>");
     }
-
   }
+
 }
---
+++ src/main/java/com/malise/home/Home.java
@@ -0,0 +1,24 @@
+package com.malise.home;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/home")
+public class Home extends HttpServlet {
+
+  @Override
+  protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    RequestDispatcher dispatcher = req.getRequestDispatcher("./app/home.html");
+    dispatcher.forward(req, resp);
+
+  }
+
+}
