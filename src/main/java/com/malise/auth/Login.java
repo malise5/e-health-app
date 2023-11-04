44f055f4ebdee2eb36edc04f86856d92ad3f0373
Halkano Malise <kudezx@gmail.com>
2023-11-04 13:21:08 +0300

    Http Session
    created new Http session

--- src/main/java/com/malise/auth/Login.java
+++ src/main/java/com/malise/auth/Login.java
@@ -6,22 +6,27 @@ import java.io.PrintWriter;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebInitParam;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 @WebServlet(urlPatterns = "/login")
 public class Login extends HttpServlet {
 
   @Override
   protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    HttpSession httpSession = req.getSession();
     resp.sendRedirect("./");
   }
 
   @Override
   protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    HttpSession httpSession = req.getSession(true);
+    httpSession.setAttribute("LoginId", "Admin");
 
     ServletContext ctx = getServletContext();
 
