22b89382ca2a7e937fa0d317de0bee25d8155afa
Halkano Malise <kudezx@gmail.com>
2023-11-05 12:38:04 +0300

    -User Registration

---
+++ src/main/java/com/malise/action/UserAction.java
@@ -0,0 +1,33 @@
+package com.malise.action;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import com.malise.app.model.entity.User;
+import com.malise.database.Database;
+
+@WebServlet("/user")
+public class UserAction extends HttpServlet {
+
+  @Override
+  protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    Database database = Database.getDbInstance();
+
+    String username = req.getParameter("username");
+    String password = req.getParameter("password");
+    String confirm_password = req.getParameter("confirm_password");
+
+    if (password.equals(confirm_password)) {
+      database.getUsers().add(new User(200L, username, password));
+    }
+
+    resp.sendRedirect("./");
+
+  }
+
+}
